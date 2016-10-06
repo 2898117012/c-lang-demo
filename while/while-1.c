b9ad302d87d1168b7aab3bed375fcb9d69a0c470
jabber <2898117012@qq.com>
2016-10-06 11:23:28 +0800

    Create while-1.c

---
+++ while/while-1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void) {
+    int i = 10;
+    while(--i){
+        printf("%d\n",i);
+    }
+	return 0;
+}
+/***
+ * output
+ * 9
+ * 8
+ * 7
+ * 6
+ * 5
+ * 4
+ * 3
+ * 2
+ * 1
+ ***/
